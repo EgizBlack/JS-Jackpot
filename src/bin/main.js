94f92a96d9986ce7d63101f4d8c95e53e34f80d6
Marcel Mares <marcelko1235@gmail.com>
2018-04-08 17:04:05 +0200

    I think it is complete, maybe will add soemthing later

--- src/bin/main.js
+++ src/bin/main.js
@@ -1,1 +1,12 @@
-console.log('hello');
+let numbers = [];
+
+function generateNumber (){
+  for (i = 0; i <5; i++){
+    numbers[i] = Math.floor(Math.random() * 98) + 1;
+  }
+  for (i = 5; i <7; i++){
+    numbers[i] = Math.floor(Math.random() * 9) + 1;
+  }
+  document.getElementById('output').innerHTML = numbers[0] + ' , ' + numbers[1] + ' , ' + numbers[2] + ' , ' + numbers[3] + ' , ' + numbers[4] +
+  ' , ' + '<span class="lastTwo">' + numbers[5] + '</span>'+' , ' + '<span class = "lastTwo">'+ numbers[6] + '<span>';
+}
